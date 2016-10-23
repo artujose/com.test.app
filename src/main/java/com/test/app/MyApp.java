204995f21c9afd45765511523d0a6c08d9820091
Arturo Diaz <artu_jose@yahoo.com.mx>
2016-10-23 15:49:13 -0500

    Modify text message of the app

--- src/main/java/com/test/app/MyApp.java
+++ src/main/java/com/test/app/MyApp.java
@@ -3,6 +3,6 @@ package com.test.app;
 public class MyApp {
 
 	public static void main(String args[]){
-		System.out.println("My Appp is running fine !! ");
+		System.out.println("My Java Standalone Application is running fine !!! ");
 	}
 }
